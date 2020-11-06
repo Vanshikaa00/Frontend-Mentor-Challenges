b5a0c8263b71366819c9b7c7ba75a9e591274045
Vanshikaa <vanshika31032000@gmail.com>
2020-11-06 22:18:00 +0530

    added 2 solutions

---
+++ Article-preview-challenge-master/styles.js
@@ -0,0 +1,11 @@
+function displayit() {
+    document.getElementById("myid").style.display = "block";
+}
+
+function displayit2() {
+    document.getElementById("containers").style.display = "none";
+    document.getElementById("myid2").style.display = "block";
+    var x = document.getElementById("c3id");
+    x.style.borderBottomLeftRadius = 0;
+    x.style.borderBottomRightRadius = 0;
+}
